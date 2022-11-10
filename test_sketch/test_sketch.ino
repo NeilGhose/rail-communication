00766b44b978bb35ffe116caa33c8abbaae5bc18
NeilGhose <neil@ghose.us>
2022-11-10 01:27:44 -0600

    removed debugging print statements from aruino and updated file.txt

--- test_sketch/test_sketch.ino
+++ test_sketch/test_sketch.ino
@@ -7,9 +7,6 @@ char data[1400];
 void loop() {
   if (Serial.available() > 0) {
     int n = Serial.readBytes(data, 1024);
-    Serial.print("You sent me ");
-    Serial.print(n);
-    Serial.println(" bytes\n");
-    Serial.println(data);
+    Serial.print(data);
   }
 }
