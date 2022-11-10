c3728624f318e9cdeb00bf62632006c27ef3eef9
NeilGhose <neil@ghose.us>
2022-11-10 00:18:38 -0600

    added print messages for debugging

--- test_sketch/test_sketch.ino
+++ test_sketch/test_sketch.ino
@@ -1,10 +1,15 @@
 void setup() {
   Serial.begin(115200);
 }
+
+char data[1400];
+
 void loop() {
   if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\t');
-    Serial.print("You sent me: \n");
+    int n = Serial.readBytes(data, 1024);
+    Serial.print("You sent me ");
+    Serial.print(n);
+    Serial.println(" bytes\n");
     Serial.println(data);
   }
 }
