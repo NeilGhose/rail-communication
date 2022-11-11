cb2435d0810f44f53dd0f4cb72d44482deb1b8b7
NeilGhose <neil@ghose.us>
2022-11-11 03:54:59 +0000

    added file size for printing

--- communication.py
+++ communication.py
@@ -4,19 +4,19 @@ import time
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
     ser.reset_input_buffer()
-    f = open("file.txt", 'r')
+    f = open("/home/rail-communication/rail-communication/file.txt", 'r')
 
-    #a = f.read().encode('utf-8')
-    a = f.read(1024).encode('utf-8')
-    print("a encoded")
     x = ""
     start = time.time()
+    b = 0
+    a = f.read(1024).encode('utf-8')
 
     while a:
         n = ser.write(a)
         a = f.read(1024).encode('utf-8')
         print("a written to",n," bytes")
         x += ser.read(2048).decode('utf-8').rstrip()
+        b += n
 
-    print(x)
-    print("Time: ", time.time() - start, "seconds")
+    #print(x)
+    print("Sent",b,"bytes in", time.time() - start, "seconds")
